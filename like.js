45946e55fffb27c394ec1cb0563e66165b4f2f34
yuya-bakuretsu <suzaku.3077kandai@gmail.com>
2020-05-19 22:32:03 +0900

    htmlファイルの追加及びラジオボタン実装

--- like.js
+++ like.js
@@ -1,11 +1,13 @@
 (function () {
   'use strict'
-  // フォームのIDなどを取得
-  const form_id = document.getElementById("form1");
+    // フォームのIDなどを取得
+    const button_name1 = document.getElementById("friend");
+    const button_name2 = document.getElementById("recommend");
+    const button_name3 = document.getElementById("love");
   form_id.onclick = () => {
     // 画面遷移
     // スキの種類を選択されたら値をgetして値を代入する。
-    let button_name = document.forms.button1.value;
+    let button_value = document.forms.button1.value;
     location.href("URL?name=" + button_name);
 
   }
