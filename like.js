87922b185488046d2e4181230f606e8cb6cd4866
yuya-bakuretsu <suzaku.3077kandai@gmail.com>
2020-05-21 14:36:19 +0900

    ページ遷移のメカニズム実装。 ボタン追加

--- like.js
+++ like.js
@@ -1,22 +1,52 @@
 (function () {
   'use strict'
     // フォームのIDなどを取得
-    const button_name1 = document.getElementById("friend");
-    const button_name2 = document.getElementById("recommend");
-    const button_name3 = document.getElementById("love");
-  form_id.onclick = () => {
-    // 画面遷移
-    // スキの種類を選択されたら値をgetして値を代入する。
-    let button_value = document.forms.button1.value;
-    location.href("URL?name=" + button_name);
+    const typeWindow = document.getElementById("select_type");
+    const nameWindow = document.getElementById("select_name");
+    const messageWindow = document.getElementById("select_message");
+    const nextType = document.getElementById("next_type");
+    const nextName = document.getElementById("next_name");
+    const backName = document.getElementById("back_name");
+    const backMessage = document.getElementById("back_message");
 
-  }
+    nextType.onclick = () => {
+        changeWindow("next_type");
+    }
+    nextName.onclick = () => {
+        changeWindow("next_name");
+    }
+    backName.onclick = () => {
+        changeWindow("back_name");
+    }
+    backMessage.onclick = () => {
+        changeWindow("back_message");
+    }
 
-  //フォームの名前をget
 
-  //メッセージをget
-
-  // ツイート機能
-
+    function changeWindow(flag){
+//        flag0ならカードのウィンドウを消す。1ならネームのウィンドウを消す。
+        switch(flag){
+            case "next_type":
+//              ココでカードのウィンドウ消す
+                typeWindow.style.display = "none";
+                nameWindow.style.display = "block";
+                break;
+            case "next_name":
+//              ネームのウィンドウを消す
+                nameWindow.style.display = "none";
+                messageWindow.style.display = "block";
+                break;
+            case "back_name":
+                typeWindow.style.display = "block";
+                nameWindow.style.display = "none";
+                break;
+            case "back_message":
+                nameWindow.style.display = "block";
+                messageWindow.style.display = "none";
+                break;
+            default:
+                break;
+        }
+    }
 
 })();
