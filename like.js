afa44b8cc37deefc72da4100dca8dcdab59afd5e
yuya-bakuretsu <suzaku.3077kandai@gmail.com>
2020-05-23 23:39:05 +0900

    default値の追加

--- like.js
+++ like.js
@@ -10,6 +10,7 @@
     const selectWindow = document.getElementById("select_window");
     const sukiLink1 = document.getElementById("link1");
     const sukiLink2 = document.getElementById("link2");
+    const backType = document.getElementById("back_type");
     const nextType = document.getElementById("next_type");
 //    const nextName = document.getElementById("next_name");
 //    const backName = document.getElementById("back_name");
@@ -17,13 +18,21 @@
     const friends = document.getElementById("friends");
     const fun = document.getElementById("fun");
     const family = document.getElementById("family");
+    const longing = document.getElementById("longing");
     const inputName = document.getElementById("input_name");
     const inputMessage = document.getElementById("input_message");
     const resetButton = document.getElementById("reset");
     const nextMessage = document.getElementById("next_message");
+    const displayPreview = document.getElementById("display_preview");
+
+
+    backType.onclick = () => {
+        changeWindow("back_type");
+    }
 
     nextType.onclick = () => {
         changeWindow("next_type");
+        loadImage(document.getElementById("form_input").type.value + ".svg");
     }
     backInput.onclick = () => {
         changeWindow("back_input");
@@ -36,6 +45,9 @@
     }
     family.onclick = () => {
         loadImage("card_family.png");
+    }
+    longing.onclick = () => {
+        loadImage("card_longing.png");
     }
     sukiLink1.onclick = () => {
         changeWindow("link1");
@@ -55,7 +67,12 @@
         inputName.value = "";
     }
     nextMessage.onclick = () => {
-        drawText();
+        let cardType = document.getElementById("form_input").type.value;
+        drawText(cardType);
+    }
+    window.onload = () =>{
+        loadImage("card_friend.png");
+        drawText(document.getElementById("form_input").type.value);
     }
 
 
@@ -64,6 +81,12 @@
 
     function changeWindow(flag){
         switch(flag){
+
+            case "back_type":
+                intro.style.display = "block";
+                typeWindow.style.display = "none";
+                break;
+
             case "next_type":
 //              ココでカードのウィンドウ消す
                 typeWindow.style.display = "none";
@@ -103,7 +126,8 @@
 
     const canvas = document.getElementById("preview");
     const ctx = canvas.getContext('2d');
-    function drawText(){
+    function drawText(cardType){
+        loadImage(cardType + ".svg");
         let text_name = inputName.value;
         let text_message = inputMessage.value;
 //      文字スタイル指定
@@ -113,7 +137,21 @@
         let y_name = (canvas.height / 3);
         let x_message = (canvas.width / 2);
         let y_message = (canvas.height / 2);
-        ctx.fillText(text_name, x_name, y_name);
+        switch(cardType){
+            case "card_friend":
+                cardType = "が友達として";
+                break;
+            case "card_family":
+                cardType = "が家族として";
+                break;
+            case "card_fun":
+                cardType = "が推しとして";
+                break;
+            case "card_longing":
+                cardType = "が憧れとして";
+                break;
+        }
+        ctx.fillText(text_name + cardType, x_name, y_name);
         ctx.fillText(text_message, x_message, y_message);
     }
 })();
